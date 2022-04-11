a918e6fd76cbad543b07cb34277a0c8c248f57cf
moistCake <94129200+moistCake@users.noreply.github.com>
2022-04-10 23:40:21 -0400

    Completed Yellow Area, Started Rate TA, TA management (select course and partly perf log), and TA admin page (#7)
    * Completed Yellow Area, Started Rate TA, TA management (select course and partly perf log), and TA admin page
    
    * New database
    
    * Code refactoring

--- session.py
+++ session.py
@@ -1,9 +1,17 @@
 from datetime import datetime, timedelta
 from database import *
 import secrets
+import enum
 
 EXPIRY_TIME = timedelta(minutes = 10)
 TIMEOUT = timedelta(minutes = 30)
+
+class User(enum.Enum):
+    admin = "admins"
+    sys_op = "sys_ops"
+    student = "students"
+    ta = "tas"
+    prof = "profs"
 
 def generate_key():
     return secrets.token_urlsafe(16)
@@ -33,3 +41,39 @@ def verify_session(session_id):
     else:
         mutate_db('UPDATE sessions SET last_activity = ? WHERE session_id = ?', [now, session_id])
         return session_id
+
+
+def get_user_id(session_id):
+    query = query_db(f"Select user_id FROM sessions WHERE session_id = '{session_id}' ")
+    user_id = None
+    for value in query:
+        user_id = dict(value).get("user_id")
+    return user_id
+
+
+def has_access_to_orange(session_id):
+    return check_group(User.admin,session_id) or check_group(User.sys_op,session_id)
+
+
+def has_access_to_blue(session_id):
+    return check_group(User.prof,session_id) or check_group(User.admin,session_id) or check_group(User.ta,session_id) or check_group(User.sys_op,session_id)
+
+
+def check_group(user_group,session_id):
+    '''
+    Verify if the user with the session id is in user_group
+    :param user_group: User enum
+    :param session_id: session_id of the user
+    :return: True if the user is part of the group
+    '''
+
+    user_id = get_user_id(session_id)
+    if user_id is None:
+        return False
+
+    query = query_db(f"Select user_id FROM {user_group.value} WHERE user_id = {user_id} ")
+    in_group = None
+    for value in query:
+        in_group = dict(value).get("user_id")
+
+    return in_group is not None
